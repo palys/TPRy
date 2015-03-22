05cad8f3af06f8201bc411a4af9790339dc8b816
potoczny <natalia.potoczny@gmail.com>
2015-03-23 00:09:48 +0100

    Create lab2.sh

---
+++ lab2/c/lab2.sh
@@ -0,0 +1,24 @@
+echo `mpiexec.hydra -machinefile ./mpihosts -np 2 ./lab2 10000 1000 2`;
+echo `mpiexec.hydra -machinefile ./mpihosts -np 3 ./lab2 10000 1000 3`;
+echo `mpiexec.hydra -machinefile ./mpihosts -np 4 ./lab2 10000 1000 4`;
+echo `mpiexec.hydra -machinefile ./mpihosts -np 5 ./lab2 10000 1000 5`;
+echo `mpiexec.hydra -machinefile ./mpihosts -np 6 ./lab2 10000 1000 6`;
+echo `mpiexec.hydra -machinefile ./mpihosts -np 7 ./lab2 10000 1000 7`;
+echo `mpiexec.hydra -machinefile ./mpihosts -np 8 ./lab2 10000 1000 8`;
+echo `mpiexec.hydra -machinefile ./mpihosts -np 9 ./lab2 10000 1000 9`;
+echo `mpiexec.hydra -machinefile ./mpihosts -np 10 ./lab2 10000 1000 10`;
+echo `mpiexec.hydra -machinefile ./mpihosts -np 11 ./lab2 10000 1000 11`;
+echo `mpiexec.hydra -machinefile ./mpihosts -np 12 ./lab2 10000 1000 12`;
+
+
+echo `mpiexec.hydra -machinefile ./mpihosts -np 2 ./lab2 10000 1000 2`;
+echo `mpiexec.hydra -machinefile ./mpihosts -np 3 ./lab2 10000 1000 3`;
+echo `mpiexec.hydra -machinefile ./mpihosts -np 4 ./lab2 10000 1000 2`;
+echo `mpiexec.hydra -machinefile ./mpihosts -np 5 ./lab2 10000 1000 3`;
+echo `mpiexec.hydra -machinefile ./mpihosts -np 6 ./lab2 10000 1000 3`;
+echo `mpiexec.hydra -machinefile ./mpihosts -np 7 ./lab2 10000 1000 4`;
+echo `mpiexec.hydra -machinefile ./mpihosts -np 8 ./lab2 10000 1000 4`;
+echo `mpiexec.hydra -machinefile ./mpihosts -np 9 ./lab2 10000 1000 5`;
+echo `mpiexec.hydra -machinefile ./mpihosts -np 10 ./lab2 10000 1000 5`;
+echo `mpiexec.hydra -machinefile ./mpihosts -np 11 ./lab2 10000 1000 6`;
+echo `mpiexec.hydra -machinefile ./mpihosts -np 12 ./lab2 10000 1000 6`;
